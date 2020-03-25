ad598959c93af05912d545a8ab69fbd1b50a51cf
abatsakidis <a.batsakidis@re-think.gr>
2020-03-25 14:06:46 +0200

    Version 1.1

--- Covid-19Sharp/Covid-19Sharp/Main.Designer.cs
+++ Covid-19Sharp/Covid-19Sharp/Main.Designer.cs
@@ -30,10 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.txtResults = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.cbcountry = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -47,50 +47,50 @@
             this.txtResults.Size = new System.Drawing.Size(260, 184);
             this.txtResults.TabIndex = 0;
             // 
-            // button1
+            // btnCheck
             // 
-            this.button1.BackColor = System.Drawing.Color.Coral;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(292, 141);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Check";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnCheck.BackColor = System.Drawing.Color.Coral;
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheck.Location = new System.Drawing.Point(292, 124);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
+            // cbcountry
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(95, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(69, 21);
-            this.comboBox1.TabIndex = 2;
+            this.cbcountry.FormattingEnabled = true;
+            this.cbcountry.Location = new System.Drawing.Point(98, 12);
+            this.cbcountry.Name = "cbcountry";
+            this.cbcountry.Size = new System.Drawing.Size(174, 21);
+            this.cbcountry.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Location = new System.Drawing.Point(10, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Select Country: ";
             // 
-            // button2
+            // btnExit
             // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(292, 181);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(292, 181);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(282, 39);
+            this.pictureBox1.Location = new System.Drawing.Point(278, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 86);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -104,7 +104,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Version 1.0";
+            this.label2.Text = "Version 1.1";
             // 
             // Main
             // 
@@ -113,10 +113,10 @@
             this.ClientSize = new System.Drawing.Size(390, 231);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbcountry);
+            this.Controls.Add(this.btnCheck);
             this.Controls.Add(this.txtResults);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -133,10 +133,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtResults;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.ComboBox cbcountry;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
     }
--- Covid-19Sharp/Covid-19Sharp/Main.cs
+++ Covid-19Sharp/Covid-19Sharp/Main.cs
@@ -1,12 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using CovidSharp;
 
@@ -14,9 +7,271 @@ namespace Covid_19Sharp
 {
     public partial class Main : Form
     {
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void load_countries()
+        {
+            Dictionary<string, string> CountriesList = new Dictionary<string, string>();
+
+            CountriesList.Add("AF", "Afghanistan");
+            CountriesList.Add("AL", "Albania");
+            CountriesList.Add("DZ", "Algeria");
+            CountriesList.Add("AS", "American Samoa");
+            CountriesList.Add("AD", "Andorra");
+            CountriesList.Add("AO", "Angola");
+            CountriesList.Add("AI", "Anguilla");
+            CountriesList.Add("AQ", "Antarctica");
+            CountriesList.Add("AG", "Antigua and Barbuda");
+            CountriesList.Add("AR", "Argentina");
+            CountriesList.Add("AM", "Armenia");
+            CountriesList.Add("AW", "Aruba");
+            CountriesList.Add("AU", "Australia");
+            CountriesList.Add("AT", "Austria");
+            CountriesList.Add("AZ", "Azerbaijan");
+            CountriesList.Add("BS", "Bahamas (the)");
+            CountriesList.Add("BH", "Bahrain");
+            CountriesList.Add("BD", "Bangladesh");
+            CountriesList.Add("BB", "Barbados");
+            CountriesList.Add("BY", "Belarus");
+            CountriesList.Add("BE", "Belgium");
+            CountriesList.Add("BZ", "Belize");
+            CountriesList.Add("BJ", "Benin");
+            CountriesList.Add("BM", "Bermuda");
+            CountriesList.Add("BT", "Bhutan");
+            CountriesList.Add("BO", "Bolivia (Plurinational State of)");
+            CountriesList.Add("BQ", "Bonaire, Sint Eustatius and Saba");
+            CountriesList.Add("BA", "Bosnia and Herzegovina");
+            CountriesList.Add("BW", "Botswana");
+            CountriesList.Add("BV", "Bouvet Island");
+            CountriesList.Add("BR", "Brazil");
+            CountriesList.Add("IO", "British Indian Ocean Territory (the)");
+            CountriesList.Add("BN", "Brunei Darussalam");
+            CountriesList.Add("BG", "Bulgaria");
+            CountriesList.Add("BF", "Burkina Faso");
+            CountriesList.Add("BI", "Burundi");
+            CountriesList.Add("CV", "Cabo Verde");
+            CountriesList.Add("KH", "Cambodia");
+            CountriesList.Add("CM", "Cameroon");
+            CountriesList.Add("CA", "Canada");
+            CountriesList.Add("KY", "Cayman Islands (the)");
+            CountriesList.Add("CF", "Central African Republic (the)");
+            CountriesList.Add("TD", "Chad");
+            CountriesList.Add("CL", "Chile");
+            CountriesList.Add("CN", "China");
+            CountriesList.Add("CX", "Christmas Island");
+            CountriesList.Add("CC", "Cocos (Keeling) Islands (the)");
+            CountriesList.Add("CO", "Colombia");
+            CountriesList.Add("KM", "Comoros (the)");
+            CountriesList.Add("CD", "Congo (the Democratic Republic of the)");
+            CountriesList.Add("CG", "Congo (the)");
+            CountriesList.Add("CK", "Cook Islands (the)");
+            CountriesList.Add("CR", "Costa Rica");
+            CountriesList.Add("HR", "Croatia");
+            CountriesList.Add("CU", "Cuba");
+            CountriesList.Add("CW", "Curaçao");
+            CountriesList.Add("CY", "Cyprus");
+            CountriesList.Add("CZ", "Czechia");
+            CountriesList.Add("CI", "Côte d'Ivoire");
+            CountriesList.Add("DK", "Denmark");
+            CountriesList.Add("DJ", "Djibouti");
+            CountriesList.Add("DM", "Dominica");
+            CountriesList.Add("DO", "Dominican Republic (the)");
+            CountriesList.Add("EC", "Ecuador");
+            CountriesList.Add("EG", "Egypt");
+            CountriesList.Add("SV", "El Salvador");
+            CountriesList.Add("GQ", "Equatorial Guinea");
+            CountriesList.Add("ER", "Eritrea");
+            CountriesList.Add("EE", "Estonia");
+            CountriesList.Add("SZ", "Eswatini");
+            CountriesList.Add("ET", "Ethiopia");
+            CountriesList.Add("FK", "Falkland Islands (the) [Malvinas]");
+            CountriesList.Add("FO", "Faroe Islands (the)");
+            CountriesList.Add("FJ", "Fiji");
+            CountriesList.Add("FI", "Finland");
+            CountriesList.Add("FR", "France");
+            CountriesList.Add("GF", "French Guiana");
+            CountriesList.Add("PF", "French Polynesia");
+            CountriesList.Add("TF", "French Southern Territories (the)");
+            CountriesList.Add("GA", "Gabon");
+            CountriesList.Add("GM", "Gambia (the)");
+            CountriesList.Add("GE", "Georgia");
+            CountriesList.Add("DE", "Germany");
+            CountriesList.Add("GH", "Ghana");
+            CountriesList.Add("GI", "Gibraltar");
+            CountriesList.Add("GR", "Greece");
+            CountriesList.Add("GL", "Greenland");
+            CountriesList.Add("GD", "Grenada");
+            CountriesList.Add("GP", "Guadeloupe");
+            CountriesList.Add("GU", "Guam");
+            CountriesList.Add("GT", "Guatemala");
+            CountriesList.Add("GG", "Guernsey");
+            CountriesList.Add("GN", "Guinea");
+            CountriesList.Add("GW", "Guinea-Bissau");
+            CountriesList.Add("GY", "Guyana");
+            CountriesList.Add("HT", "Haiti");
+            CountriesList.Add("HM", "Heard Island and McDonald Islands");
+            CountriesList.Add("VA", "Holy See (the)");
+            CountriesList.Add("HN", "Honduras");
+            CountriesList.Add("HK", "Hong Kong");
+            CountriesList.Add("HU", "Hungary");
+            CountriesList.Add("IS", "Iceland");
+            CountriesList.Add("IN", "India");
+            CountriesList.Add("ID", "Indonesia");
+            CountriesList.Add("IR", "Iran (Islamic Republic of)");
+            CountriesList.Add("IQ", "Iraq");
+            CountriesList.Add("IE", "Ireland");
+            CountriesList.Add("IM", "Isle of Man");
+            CountriesList.Add("IL", "Israel");
+            CountriesList.Add("IT", "Italy");
+            CountriesList.Add("JM", "Jamaica");
+            CountriesList.Add("JP", "Japan");
+            CountriesList.Add("JE", "Jersey");
+            CountriesList.Add("JO", "Jordan");
+            CountriesList.Add("KZ", "Kazakhstan");
+            CountriesList.Add("KE", "Kenya");
+            CountriesList.Add("KI", "Kiribati");
+            CountriesList.Add("KP", "Korea (the Democratic People's Republic of)");
+            CountriesList.Add("KR", "Korea (the Republic of)");
+            CountriesList.Add("KW", "Kuwait");
+            CountriesList.Add("KG", "Kyrgyzstan");
+            CountriesList.Add("LA", "Lao People's Democratic Republic (the)");
+            CountriesList.Add("LV", "Latvia");
+            CountriesList.Add("LB", "Lebanon");
+            CountriesList.Add("LS", "Lesotho");
+            CountriesList.Add("LR", "Liberia");
+            CountriesList.Add("LY", "Libya");
+            CountriesList.Add("LI", "Liechtenstein");
+            CountriesList.Add("LT", "Lithuania");
+            CountriesList.Add("LU", "Luxembourg");
+            CountriesList.Add("MO", "Macao");
+            CountriesList.Add("MG", "Madagascar");
+            CountriesList.Add("MW", "Malawi");
+            CountriesList.Add("MY", "Malaysia");
+            CountriesList.Add("MV", "Maldives");
+            CountriesList.Add("ML", "Mali");
+            CountriesList.Add("MT", "Malta");
+            CountriesList.Add("MH", "Marshall Islands (the)");
+            CountriesList.Add("MQ", "Martinique");
+            CountriesList.Add("MR", "Mauritania");
+            CountriesList.Add("MU", "Mauritius");
+            CountriesList.Add("YT", "Mayotte");
+            CountriesList.Add("MX", "Mexico");
+            CountriesList.Add("FM", "Micronesia (Federated States of)");
+            CountriesList.Add("MD", "Moldova (the Republic of)");
+            CountriesList.Add("MC", "Monaco");
+            CountriesList.Add("MN", "Mongolia");
+            CountriesList.Add("ME", "Montenegro");
+            CountriesList.Add("MS", "Montserrat");
+            CountriesList.Add("MA", "Morocco");
+            CountriesList.Add("MZ", "Mozambique");
+            CountriesList.Add("MM", "Myanmar");
+            CountriesList.Add("NA", "Namibia");
+            CountriesList.Add("NR", "Nauru");
+            CountriesList.Add("NP", "Nepal");
+            CountriesList.Add("NL", "Netherlands (the)");
+            CountriesList.Add("NC", "New Caledonia");
+            CountriesList.Add("NZ", "New Zealand");
+            CountriesList.Add("NI", "Nicaragua");
+            CountriesList.Add("NE", "Niger (the)");
+            CountriesList.Add("NG", "Nigeria");
+            CountriesList.Add("NU", "Niue");
+            CountriesList.Add("NF", "Norfolk Island");
+            CountriesList.Add("MP", "Northern Mariana Islands (the)");
+            CountriesList.Add("NO", "Norway");
+            CountriesList.Add("OM", "Oman");
+            CountriesList.Add("PK", "Pakistan");
+            CountriesList.Add("PW", "Palau");
+            CountriesList.Add("PS", "Palestine, State of");
+            CountriesList.Add("PA", "Panama");
+            CountriesList.Add("PG", "Papua New Guinea");
+            CountriesList.Add("PY", "Paraguay");
+            CountriesList.Add("PE", "Peru");
+            CountriesList.Add("PH", "Philippines (the)");
+            CountriesList.Add("PN", "Pitcairn");
+            CountriesList.Add("PL", "Poland");
+            CountriesList.Add("PT", "Portugal");
+            CountriesList.Add("PR", "Puerto Rico");
+            CountriesList.Add("QA", "Qatar");
+            CountriesList.Add("MK", "Republic of North Macedonia");
+            CountriesList.Add("RO", "Romania");
+            CountriesList.Add("RU", "Russian Federation (the)");
+            CountriesList.Add("RW", "Rwanda");
+            CountriesList.Add("RE", "Réunion");
+            CountriesList.Add("BL", "Saint Barthélemy");
+            CountriesList.Add("SH", "Saint Helena, Ascension and Tristan da Cunha");
+            CountriesList.Add("KN", "Saint Kitts and Nevis");
+            CountriesList.Add("LC", "Saint Lucia");
+            CountriesList.Add("MF", "Saint Martin (French part)");
+            CountriesList.Add("PM", "Saint Pierre and Miquelon");
+            CountriesList.Add("VC", "Saint Vincent and the Grenadines");
+            CountriesList.Add("WS", "Samoa");
+            CountriesList.Add("SM", "San Marino");
+            CountriesList.Add("ST", "Sao Tome and Principe");
+            CountriesList.Add("SA", "Saudi Arabia");
+            CountriesList.Add("SN", "Senegal");
+            CountriesList.Add("RS", "Serbia");
+            CountriesList.Add("SC", "Seychelles");
+            CountriesList.Add("SL", "Sierra Leone");
+            CountriesList.Add("SG", "Singapore");
+            CountriesList.Add("SX", "Sint Maarten (Dutch part)");
+            CountriesList.Add("SK", "Slovakia");
+            CountriesList.Add("SI", "Slovenia");
+            CountriesList.Add("SB", "Solomon Islands");
+            CountriesList.Add("SO", "Somalia");
+            CountriesList.Add("ZA", "South Africa");
+            CountriesList.Add("GS", "South Georgia and the South Sandwich Islands");
+            CountriesList.Add("SS", "South Sudan");
+            CountriesList.Add("ES", "Spain");
+            CountriesList.Add("LK", "Sri Lanka");
+            CountriesList.Add("SD", "Sudan (the)");
+            CountriesList.Add("SR", "Suriname");
+            CountriesList.Add("SJ", "Svalbard and Jan Mayen");
+            CountriesList.Add("SE", "Sweden");
+            CountriesList.Add("CH", "Switzerland");
+            CountriesList.Add("SY", "Syrian Arab Republic");
+            CountriesList.Add("TW", "Taiwan (Province of China)");
+            CountriesList.Add("TJ", "Tajikistan");
+            CountriesList.Add("TZ", "Tanzania, United Republic of");
+            CountriesList.Add("TH", "Thailand");
+            CountriesList.Add("TL", "Timor-Leste");
+            CountriesList.Add("TG", "Togo");
+            CountriesList.Add("TK", "Tokelau");
+            CountriesList.Add("TO", "Tonga");
+            CountriesList.Add("TT", "Trinidad and Tobago");
+            CountriesList.Add("TN", "Tunisia");
+            CountriesList.Add("TR", "Turkey");
+            CountriesList.Add("TM", "Turkmenistan");
+            CountriesList.Add("TC", "Turks and Caicos Islands (the)");
+            CountriesList.Add("TV", "Tuvalu");
+            CountriesList.Add("UG", "Uganda");
+            CountriesList.Add("UA", "Ukraine");
+            CountriesList.Add("AE", "United Arab Emirates (the)");
+            CountriesList.Add("GB", "United Kingdom of Great Britain and Northern Ireland (the)");
+            CountriesList.Add("UM", "United States Minor Outlying Islands (the)");
+            CountriesList.Add("US", "United States of America (the)");
+            CountriesList.Add("UY", "Uruguay");
+            CountriesList.Add("UZ", "Uzbekistan");
+            CountriesList.Add("VU", "Vanuatu");
+            CountriesList.Add("VE", "Venezuela (Bolivarian Republic of)");
+            CountriesList.Add("VN", "Viet Nam");
+            CountriesList.Add("VG", "Virgin Islands (British)");
+            CountriesList.Add("VI", "Virgin Islands (U.S.)");
+            CountriesList.Add("WF", "Wallis and Futuna");
+            CountriesList.Add("EH", "Western Sahara");
+            CountriesList.Add("YE", "Yemen");
+            CountriesList.Add("ZM", "Zambia");
+            CountriesList.Add("ZW", "Zimbabwe");
+            CountriesList.Add("AX", "Åland Islands");
+
+
+            cbcountry.DataSource = new BindingSource(CountriesList, null);
+            cbcountry.DisplayMember = "Value";
+            cbcountry.ValueMember = "Key";
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,7 +286,7 @@ namespace Covid_19Sharp
 
             txtResults.Text += "Global";
             txtResults.Text += "\r\n";
-            txtResults.Text += "~~~~~~";
+            txtResults.Text += "~~~~";
             txtResults.Text += "\r\n";
             txtResults.Text += "Confirmed: ";
             txtResults.Text += data.LatestConfirmed() + "\r\n";
@@ -41,37 +296,30 @@ namespace Covid_19Sharp
 
             txtResults.Text += "Deaths: ";
             txtResults.Text += data.LatestDeaths() + "\r\n";
-
-
             txtResults.Text += "\r\n";
 
-            txtResults.Text += comboBox1.SelectedItem.ToString();
+            txtResults.Text += "Country - " + cbcountry.SelectedValue.ToString();
+            txtResults.Text += "\r\n";
+            txtResults.Text += "~~~~~~~~~";
+            txtResults.Text += "\r\n";
+
+            txtResults.Text += cbcountry.SelectedValue.ToString();
             txtResults.Text += " Confirmed: ";
-            txtResults.Text += data.FromCountryConfirmed(comboBox1.SelectedItem.ToString()) + "\r\n";
+            txtResults.Text += data.FromCountryConfirmed(cbcountry.SelectedValue.ToString()) + "\r\n";
 
-
-            txtResults.Text += comboBox1.SelectedItem.ToString();
+            txtResults.Text += cbcountry.SelectedValue.ToString();
             txtResults.Text += " Recovered: ";
-            txtResults.Text += data.FromCountryRecovered(comboBox1.SelectedItem.ToString()) + "\r\n";
+            txtResults.Text += data.FromCountryRecovered(cbcountry.SelectedValue.ToString()) + "\r\n";
 
-
-            txtResults.Text += comboBox1.SelectedItem.ToString();
+            txtResults.Text += cbcountry.SelectedValue.ToString();
             txtResults.Text += " Deaths: ";
-            txtResults.Text += data.FromCountryDeaths(comboBox1.SelectedItem.ToString()) + "\r\n";
-
+            txtResults.Text += data.FromCountryDeaths(cbcountry.SelectedValue.ToString()) + "\r\n";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string[] lineOfContents = File.ReadAllLines("countries.txt");
-            foreach (var line in lineOfContents)
-            {
-                string[] tokens = line.Split(',');
-                comboBox1.Items.Add(tokens[0]);
-            }
-
-            comboBox1.SelectedIndex = comboBox1.FindStringExact("GR");
-
+            load_countries();
+            cbcountry.SelectedIndex = cbcountry.FindStringExact("Greece");
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -79,4 +327,6 @@ namespace Covid_19Sharp
             Application.Exit();
         }
     }
+
+ 
 }
